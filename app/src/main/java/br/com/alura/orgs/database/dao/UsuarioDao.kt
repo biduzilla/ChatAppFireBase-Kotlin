7da7e282bd68644dca8e808a0a8776a1401a181c
biduzilla <rlyowlly19@gmail.com>
2023-04-05 14:39:13 -0300

    auth pelo bd room

--- app/src/main/java/br/com/alura/orgs/database/dao/UsuarioDao.kt
+++ app/src/main/java/br/com/alura/orgs/database/dao/UsuarioDao.kt
@@ -2,11 +2,19 @@ package br.com.alura.orgs.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import br.com.alura.orgs.model.Usuario
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UsuarioDao {
 
     @Insert
     suspend fun salvar(usuario: Usuario)
+
+    @Query("SELECT * FROM Usuario WHERE id = :usuarioId AND senha=:senha")
+    suspend fun auth(usuarioId: String, senha: String): Usuario?
+
+    @Query("SELECT * FROM Usuario WHERE id = :usuarioId")
+    fun buscaPorId(usuarioId: String):Flow<Usuario>
 }
