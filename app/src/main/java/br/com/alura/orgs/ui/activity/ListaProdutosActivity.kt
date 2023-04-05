f50f0486b06de2188891ac14cd4589f5dca23b16
biduzilla <rlyowlly19@gmail.com>
2023-04-05 16:15:11 -0300

    criação base activity user

--- app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
@@ -5,28 +5,21 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
-import androidx.datastore.preferences.core.edit
-
 import androidx.lifecycle.lifecycleScope
 import br.com.alura.orgs.R
 import br.com.alura.orgs.database.AppDatabase
 import br.com.alura.orgs.databinding.ActivityListaProdutosActivityBinding
-
-import br.com.alura.orgs.extensions.vaiPara
-import br.com.alura.orgs.preferences.dataStore
-import br.com.alura.orgs.preferences.usuarioLogadoPreferences
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.launch
 
 
-class ListaProdutosActivity : AppCompatActivity() {
+class ListaProdutosActivity : UsuarioBaseActivity() {
 
     private val adapter = ListaProdutosAdapter(context = this)
+
     private val binding by lazy {
         ActivityListaProdutosActivityBinding.inflate(layoutInflater)
     }
@@ -35,38 +28,20 @@ class ListaProdutosActivity : AppCompatActivity() {
         db.produtoDao()
     }
 
-    private val usuarioDao by lazy {
-        AppDatabase.instancia(this).usuarioDao()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         configuraRecyclerView()
         configuraFab()
 
-
         lifecycleScope.launch {
             launch {
-                verificaUserLogado()
-            }
-        }
-    }
-
-    private suspend fun verificaUserLogado() {
-        dataStore.data.collect { preferences ->
-            preferences[usuarioLogadoPreferences]?.let { usuarioId ->
-                buscaUser(usuarioId)
-            } ?: vaiParaLogin()
-        }
-    }
-
-    private fun buscaUser(usuarioId: String) {
-        lifecycleScope.launch {
-            usuarioDao.buscaPorId(usuarioId).firstOrNull()?.let {
-                launch {
-                    buscaProdutosUser()
-                }
+                usuario
+                    .filterNotNull()
+                    .collect {
+                        Log.i("infoteste", "onCreate: $it")
+                        buscaProdutosUser()
+                    }
             }
         }
     }
@@ -77,10 +52,8 @@ class ListaProdutosActivity : AppCompatActivity() {
         }
     }
 
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_lista_produtos, menu)
-
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -88,22 +61,11 @@ class ListaProdutosActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.menu_sair -> {
                 lifecycleScope.launch {
-                    deslogarUser()
+                    deslogaUsuario()
                 }
             }
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    private suspend fun deslogarUser() {
-        dataStore.edit { preferences ->
-            preferences.remove(usuarioLogadoPreferences)
-        }
-    }
-
-    private fun vaiParaLogin() {
-        vaiPara(LoginActivity::class.java)
-        finish()
     }
 
     private fun configuraFab() {
@@ -131,5 +93,4 @@ class ListaProdutosActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
 }
